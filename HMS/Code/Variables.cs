5400985452c34b2f4ec23d8fc127ee5ba32aaf4d
arif3592 <khalilurr412@gmail.com>
2019-09-21 00:51:09 +0600

    Front end & User Authentication Work

---
+++ HMS/Code/Variables.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HMS.Code
+{
+    public class Variables
+    {
+        public const string HotelFullName = "Hotel Bangladesh";
+        public const string HotelShortName = "Hotel BD";
+        public const string HotelAvrabiateName = "HBD";
+    }
+}
